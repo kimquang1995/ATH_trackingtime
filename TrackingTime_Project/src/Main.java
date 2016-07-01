62f0986dc2c2636c4bec052cd4ad7a2e3f507a78
kimquang1995 <kimquang1995@gmail.com>
2016-07-01 15:23:20 +0700

    test update 3rd

--- TrackingTime_Project/src/Main.java
+++ TrackingTime_Project/src/Main.java
@@ -15,6 +15,7 @@ public class Main {
 		// TODO Auto-generated method stub
 		System.out.println("Hello world");
 		System.out.println("Hello world update");
+		System.out.println("Hello world update 3rd");
 	}
 
 }
