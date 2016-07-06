d6515902714a73b4e6db877a98d89bf1deb966c6
ThanhPhuc1610 <thanhphuc1610@gmail.com>
2016-07-06 16:04:59 +0700

    Update chuc nang login

--- TrackingTime_Project/src/Login_Interface.java
+++ TrackingTime_Project/src/Login_Interface.java
@@ -5,10 +5,16 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
+
 import java.awt.Font;
+
+import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.JPasswordField;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class Login_Interface extends JFrame {
@@ -18,7 +24,7 @@ public class Login_Interface extends JFrame {
 	private JButton btnForgotPassword;
 	private JButton btnOk;
 	private JButton btnCancel;
-	private JPasswordField pwdPassword;
+	private JTextField pwdPassword;
 
 	/**
 	 * Launch the application.
@@ -39,6 +45,8 @@ public class Login_Interface extends JFrame {
 	/**
 	 * Create the frame.
 	 */
+	private static String USER = "ThanhPhuc1610";
+	private static String PASS = "thanhphuc1610";
 	public Login_Interface() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
@@ -58,19 +66,46 @@ public class Login_Interface extends JFrame {
 		contentPane.add(txtUsername);
 		txtUsername.setColumns(10);
 		
+		
+		
 		btnForgotPassword = new JButton("Forgot Password");
 		btnForgotPassword.setBounds(212, 155, 134, 23);
 		contentPane.add(btnForgotPassword);
 		
 		btnOk = new JButton("Ok");
+		btnOk.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+				if (txtUsername.getText().equals("Type your Username here!") || pwdPassword.getText().equals("")) {
+				
+				JOptionPane.showMessageDialog(null, "you need to enter your name and password", "Login Error", JOptionPane.OK_OPTION);
+				}
+				
+				if (txtUsername.getText().equals(USER) && pwdPassword.getText().equals(PASS)){
+				JOptionPane.showMessageDialog(null, "Logged", "Information", JOptionPane.INFORMATION_MESSAGE);
+				}
+				
+				else {
+				JOptionPane.showMessageDialog(null, "Login Failure", "Information", JOptionPane.OK_OPTION);
+				txtUsername.requestFocus();
+				}
+			}
+		});
 		btnOk.setBounds(68, 187, 134, 23);
 		contentPane.add(btnOk);
 		
 		btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
 		btnCancel.setBounds(212, 187, 134, 23);
 		contentPane.add(btnCancel);
 		
-		pwdPassword = new JPasswordField();
+		
+		
+		pwdPassword = new JTextField();
 		pwdPassword.setText("Password");
 		pwdPassword.setBounds(99, 123, 228, 23);
 		contentPane.add(pwdPassword);
