6df8cb3dd482478a316063a673255c8c37654e50
nghianguyent133981 <Zed@172.16.14.100>
2016-07-08 16:41:23 +0700

    update hiển thị % cho từng part trong biểu đồ

--- TrackingTime_Project/src/Statistics_demo.java
+++ TrackingTime_Project/src/Statistics_demo.java
@@ -2,6 +2,7 @@ import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.text.NumberFormat;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -10,8 +11,12 @@ import javax.swing.border.EmptyBorder;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
+import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
+import org.jfree.chart.labels.PieSectionLabelGenerator;
+import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
 import org.jfree.chart.plot.PiePlot;
+import org.jfree.chart.plot.PiePlot3D;
 import org.jfree.data.general.DefaultPieDataset;
 
 public class Statistics_demo extends JFrame {
@@ -56,8 +61,12 @@ public class Statistics_demo extends JFrame {
 			    dataset1.setValue("Relax", 25);
 			    dataset1.setValue("Work", 25);
 			    dataset1.setValue("Sleep", 25);
+			 // Create the custom label generator
+		        final PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator("{0} = {2}");
 			    JFreeChart chart1 = ChartFactory.createPieChart("Weeks", dataset1, false, false, false);
 			    PiePlot P = (PiePlot) chart1.getPlot();
+		        P.setLabelGenerator(labelGenerator);
+
 			    //P.setForegroundAlpha(0);
 			    P.setCircular(true);
 			    ChartFrame frame = new ChartFrame("Statistics in Weeks",chart1);
@@ -78,9 +87,13 @@ public class Statistics_demo extends JFrame {
 			    dataset2.setValue("Relax", 75);
 			    dataset2.setValue("Work", 324);
 			    dataset2.setValue("Sleep", 23);
+			 // Create the custom label generator
+		        final PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator("{0} = {2}");
 			    JFreeChart chart2 = ChartFactory.createPieChart("Years", dataset2, false, false, false);
 			    PiePlot P = (PiePlot) chart2.getPlot();
 			    //P.setForegroundAlpha(0);
+		        P.setLabelGenerator(labelGenerator);
+
 			    P.setCircular(true);
 			    ChartFrame frame = new ChartFrame("Statistics in Years",chart2);
 			    frame.setVisible(true);
@@ -95,18 +108,26 @@ public class Statistics_demo extends JFrame {
 		btnTagInYears.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
+				//create data
 				DefaultPieDataset dataset3 = new DefaultPieDataset();
 			    dataset3.setValue("Study", 324);
 			    dataset3.setValue("Relax", 234);
 			    dataset3.setValue("Work", 123);
 			    dataset3.setValue("Sleep", 890);
+			 // Create the custom label generator
+		        final PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator("{0} = {2}");
+
 			    JFreeChart chart3 = ChartFactory.createPieChart("Tag in Years", dataset3, false, false, false);
 			    PiePlot P = (PiePlot) chart3.getPlot();
+		        P.setLabelGenerator(labelGenerator);
+
+
 			    //P.setForegroundAlpha(0);
 			    P.setCircular(true);
 			    ChartFrame frame = new ChartFrame("Statistics Tag in Years",chart3);
 			    frame.setVisible(true);
 			    frame.setSize(350,350);
+			    
 			}
 		});
 		btnTagInYears.setBounds(279, 36, 120, 25);
