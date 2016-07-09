3bf7fdfed0a5742484a054f2312e439a37a43ce3
Bình <Bnh@DESKTOP-DGB9SM3>
2016-07-09 11:48:25 +0700

    Update TimeLog

--- TrackingTime_Project/src/TimeLog.java
+++ TrackingTime_Project/src/TimeLog.java
@@ -1,41 +1,46 @@
-import java.awt.BorderLayout;
-import java.awt.Component;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
+import javax.swing.DefaultCellEditor;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.ListSelectionModel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
-import javax.swing.event.TableModelListener;
-import javax.swing.JLabel;
-
-import java.awt.Font;
-
-import javax.swing.JComboBox;
-import javax.swing.JTextField;
-import javax.swing.JButton;
-import javax.swing.JTable;
-import javax.swing.ListSelectionModel;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
-import javax.swing.JScrollPane;
-import javax.swing.JScrollBar;
+import javax.swing.table.TableCellEditor;
 
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+import java.sql.*;
+import java.util.Arrays;
 
-import javax.swing.DefaultComboBoxModel;
+import net.proteanit.sql.DbUtils;
 
 public class TimeLog extends JFrame {
-
-	private JPanel contentPane;
+	private JPanel contentPane1;
 	private JTextField textField;
 	private JTextField txtName;
 	private JTextField txtHours;
 	private JTable table;
 	int row;
-
+	private JPanel contentPane;
+	ResultSet rs;
+	String TagName;
+	DatabaseConnection db = new DatabaseConnection();
 	/**
 	 * Launch the application.
 	 */
@@ -56,93 +61,157 @@ public class TimeLog extends JFrame {
 	 * Create the frame.
 	 */
 	public TimeLog() {
+		
 		setTitle("Time Log");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 562, 540);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
+		contentPane1 = new JPanel();
+		contentPane1.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane1);
+		contentPane1.setLayout(null);
 
 		JLabel lblTitle = new JLabel("Time Log");
 		lblTitle.setFont(new Font("Arial", Font.BOLD, 24));
 		lblTitle.setBounds(213, 13, 123, 29);
-		contentPane.add(lblTitle);
+		contentPane1.add(lblTitle);
 
 		JLabel lblDay = new JLabel("Day :");
 		lblDay.setFont(new Font("Arial", Font.BOLD, 22));
 		lblDay.setBounds(167, 55, 56, 29);
-		contentPane.add(lblDay);
+		contentPane1.add(lblDay);
 
 		JLabel lblChoseDay = new JLabel("New label");
 		lblChoseDay.setFont(new Font("Arial", Font.BOLD, 22));
 		lblChoseDay.setBounds(233, 55, 200, 29);
-		contentPane.add(lblChoseDay);
+		contentPane1.add(lblChoseDay);
 
 		JLabel lblTag = new JLabel("Tag");
 		lblTag.setFont(new Font("Arial", Font.BOLD, 20));
 		lblTag.setBounds(37, 123, 45, 29);
-		contentPane.add(lblTag);
+		contentPane1.add(lblTag);
 
 		JLabel lblName = new JLabel("Task Name");
 		lblName.setFont(new Font("Arial", Font.BOLD, 20));
 		lblName.setBounds(37, 165, 111, 29);
-		contentPane.add(lblName);
+		contentPane1.add(lblName);
 
 		JLabel lblHours = new JLabel("Hours");
 		lblHours.setFont(new Font("Arial", Font.BOLD, 20));
 		lblHours.setBounds(37, 207, 65, 29);
-		contentPane.add(lblHours);
+		contentPane1.add(lblHours);
 
 		JComboBox cmb = new JComboBox();
-		cmb.setModel(new DefaultComboBoxModel(new String[] { "H\u1ECDc",
-				"Ch\u01A1i" }));
 		cmb.setBounds(167, 128, 223, 22);
-		contentPane.add(cmb);
+		cmb.addItem("All");
+		contentPane1.add(cmb);
+		try {
+			db.Connect();
+			PreparedStatement query = db.getConnection().prepareStatement("Select * from Tag");
+			ResultSet rs = query.executeQuery();
+			while(rs.next())
+			{
+				cmb.addItem(rs.getString("Name"));
+			}
+			
+		} catch (Exception e) {
+			  JOptionPane.showMessageDialog(null, "ERROR");
+		}
+		
+		cmb.addItemListener(new ItemListener() {
+
+            @Override
+            public void itemStateChanged(ItemEvent e) {
+            	TagName = cmb.getSelectedItem().toString();
+            	try
+                {
+            		
+            		db.Connect();
+            		PreparedStatement subquery = db.getConnection().prepareStatement("Select Id from Tag where Name LIKE '"+TagName+"%'");
+        			ResultSet subrs = subquery.executeQuery();
+        			PreparedStatement query = db.getConnection().prepareStatement("Select Name,Hours,Id_Tag from TimeLog Where Id_Tag LIKE '"+subrs+"%'");
+        			ResultSet rs = query.executeQuery();
+        			table.setModel(DbUtils.resultSetToTableModel(rs));
+        			table.repaint();
+                   /* while(rs.next())
+                    {  
+                        Object[] row = new Object[columns];
+                        for (int i = 1; i <= columns; i++)
+                        {  
+                            row[i - 1] = rs.getObject(i);
+                        }
+                        ((DefaultTableModel) table.getModel()).insertRow(rs.getRow()-1,row);
+                    }*/
+                }
+                catch(Exception e1)
+                {
+                }
+            }
+        });
 
 		txtName = new JTextField();
 		txtName.setBounds(167, 170, 223, 22);
-		contentPane.add(txtName);
+		contentPane1.add(txtName);
 		txtName.setColumns(10);
 
 		txtHours = new JTextField();
 		txtHours.setBounds(167, 212, 223, 22);
-		contentPane.add(txtHours);
+		contentPane1.add(txtHours);
 		txtHours.setColumns(10);
 
 		JButton btnAdd = new JButton("Add");
 		btnAdd.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 
-				String tag = cmb.getSelectedItem().toString();
+				
 				String name = txtName.getText();
 				Double hours = Double
 						.parseDouble(txtHours.getText().toString());
-				Object[] row = { name, tag, hours };
-				DefaultTableModel model = (DefaultTableModel) table.getModel();
-				model.addRow(row);
+				try {
+					db.Connect();
+					PreparedStatement query = db.getConnection().prepareStatement("Insert into TimeLog (Name,Hours,Date,Id_Tag) values ('"+name+"','"+hours+"',GetDate(),1)");
+					ResultSet rs = query.executeQuery();
+					rs.close();
+					table.setModel(DbUtils.resultSetToTableModel(rs));
+					table.repaint();
+        			JOptionPane.showMessageDialog(null, "Success");
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				
+    			
 				txtName.setText(null);
 				txtHours.setText(null);
 
 			}
 		});
 		btnAdd.setBounds(422, 127, 97, 25);
-		contentPane.add(btnAdd);
+		contentPane1.add(btnAdd);
 
 		JButton btnEdit = new JButton("Edit");
 		btnEdit.setEnabled(false);
 		btnEdit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				row = table.getSelectedRow();
 				String name = txtName.getText();
 				Double hours = Double.parseDouble(txtHours.getText());
-				DefaultTableModel model = (DefaultTableModel) table.getModel();
-				model.setValueAt(name, row, 0);
-				model.setValueAt(hours, row, 2);
+				try {
+					db.Connect();
+					PreparedStatement query = db.getConnection().prepareStatement("Update TimeLog set Name='"+name+"',Hours='"+hours+"'");
+					ResultSet rs = query.executeQuery();
+					rs.close();
+					table.setModel(DbUtils.resultSetToTableModel(rs));
+					table.repaint();
+        			JOptionPane.showMessageDialog(null, "Success");
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
+				
+    			
+				txtName.setText(null);
+				txtHours.setText(null);
 			}
 		});
 		btnEdit.setBounds(422, 169, 97, 25);
-		contentPane.add(btnEdit);
+		contentPane1.add(btnEdit);
 
 		JButton btnDelete = new JButton("Delete");
 		btnDelete.setEnabled(false);
@@ -150,33 +219,45 @@ public class TimeLog extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 				txtName.setText(null);
 				txtHours.setText(null);
-				DefaultTableModel model = (DefaultTableModel) table.getModel();
-				// model.setValueAt(null, row, 0);
-				// model.setValueAt(null, row, 2);
-				// model.setValueAt(null, row, 1);
-				model.removeRow(row);
-				// model.removeRow(row);
+				try {
+					db.Connect();
+					PreparedStatement query = db.getConnection().prepareStatement("Delete from TimeLog where Name='"+txtName.getText().toString()+"'");
+					ResultSet rs = query.executeQuery();
+					rs.close();
+					table.setModel(DbUtils.resultSetToTableModel(rs));
+					table.repaint();
+        			JOptionPane.showMessageDialog(null, "Success");
+				} catch (Exception e1) {
+					e1.printStackTrace();
+				}
 			}
 		});
 		btnDelete.setBounds(422, 211, 97, 25);
-		contentPane.add(btnDelete);
+		contentPane1.add(btnDelete);
 
 		JScrollPane scrollPane = new JScrollPane();
 		scrollPane.setBounds(37, 261, 482, 194);
-		contentPane.add(scrollPane);
+		contentPane1.add(scrollPane);
 
 		table = new JTable();
 		scrollPane.setViewportView(table);
-		table.setModel(new DefaultTableModel(new Object[][] {
-
-		}, new String[] { "Task Name", "Tag", "Hours" }) {
-			Class[] columnTypes = new Class[] { String.class, String.class,
-					String.class };
-
-			public Class getColumnClass(int columnIndex) {
-				return columnTypes[columnIndex];
+		try {
+			db.Connect();
+			PreparedStatement query = db.getConnection().prepareStatement("Select Name,Hours,Id_Tag from TimeLog");
+			ResultSet rs = query.executeQuery();
+			table.setModel(DbUtils.resultSetToTableModel(rs));
+			for (Class c: Arrays.asList(String.class, String.class, String.class)) {
+			    TableCellEditor ce = table.getDefaultEditor(c);
+			    if (ce instanceof DefaultCellEditor) {
+			            ((DefaultCellEditor) ce).setClickCountToStart(Integer.MAX_VALUE);
+			    }
+			    
 			}
-		});
+			 
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
 		table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
 		table.setFont(new Font("Arial", Font.PLAIN, 18));
 		DefaultTableCellRenderer centerRenderer = new DefaultTableCellRenderer();
@@ -191,7 +272,7 @@ public class TimeLog extends JFrame {
 				row = table.getSelectedRow();
 				DefaultTableModel model = (DefaultTableModel) table.getModel();
 				String SelectedName = (String) model.getValueAt(row, 0);
-				Double SelectedHours = (Double) model.getValueAt(row, 2);
+				Double SelectedHours = (Double) model.getValueAt(row, 1);
 				txtName.setText(SelectedName);
 				txtHours.setText(SelectedHours.toString());
 
