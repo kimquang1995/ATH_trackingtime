ecca42936ac741b0e8e1485099e217f0ed53d624
Windows 7 <Windows 7@WIN-PARU5LD87IC>
2016-07-16 02:27:48 +0700

    Update Statistic Function

--- TrackingTime_Project/src/Statistics.java
+++ TrackingTime_Project/src/Statistics.java
@@ -1,46 +1,43 @@
 import java.awt.BorderLayout;
-import java.awt.Dimension;
+import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.text.NumberFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
+import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
-import javax.swing.JOptionPane;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.SwingConstants;
 
 import org.jfree.chart.ChartFactory;
-import org.jfree.chart.ChartFrame;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.labels.PieSectionLabelGenerator;
 import org.jfree.chart.labels.StandardPieSectionLabelGenerator;
 import org.jfree.chart.plot.PiePlot;
-import org.jfree.chart.plot.PiePlot3D;
 import org.jfree.data.general.DefaultPieDataset;
-import javax.swing.BoxLayout;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import java.awt.FlowLayout;
-import javax.swing.JComboBox;
-import javax.swing.JCheckBox;
-import javax.swing.JLabel;
-import javax.swing.JPopupMenu;
-import java.awt.Component;
-import java.awt.Font;
-import javax.swing.SwingConstants;
-import java.awt.Color;
-import javax.swing.DefaultComboBoxModel;
+import org.omg.CORBA.PUBLIC_MEMBER;
 
 public class Statistics extends JFrame {
 
 	private JPanel contentPane;
+	private String[] year = new String[100];
+	private String[] week = new String[52];
 	DatabaseConnection db = new DatabaseConnection();
+	private JComboBox cbWeek;
+	private JComboBox cbYear;
+	private JComboBox cbSta_Year;
 
 	/**
 	 * Launch the application.
@@ -72,64 +69,68 @@ public class Statistics extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 800, 600);
 		contentPane = new JPanel();
-
 		// contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		// weeks
 		// -*--------------------------------------------********************************************************************************************
 
 		JButton btnWeeks = new JButton("Show");
-		btnWeeks.setBounds(243, 73, 84, 29);
+		btnWeeks.setBounds(192, 73, 110, 25);
 
 		btnWeeks.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 
-				int year = 2016;
-				int week = 1;
-				String sQuery = "Select * from Plans where Year=" + year
-						+ " and Week =" + week;
+				int year = Integer
+						.parseInt(cbYear.getSelectedItem().toString());
+				int week = cbWeek.getSelectedIndex() + 1;
+
 				try {
 					DefaultPieDataset dataset1 = new DefaultPieDataset();
+					String sQuery = "Select * from Plans where Year=" + year
+							+ " and Week =" + week;
 					PreparedStatement query = db.getConnection()
 							.prepareStatement(sQuery);
 					ResultSet rs = query.executeQuery();
-
+					if (!rs.isBeforeFirst()) {
+						dataset1.setValue("No Values", 0);
+					}
 					// float hours = Float.parseFloat(rs.getString("Hours"));
 
-					while (rs.next()) {
-						String sTagQuery = "Select * from Tag where Id = "
-								+ Integer.parseInt(rs.getString("Id_Tag"));
-						PreparedStatement Tagquery = db.getConnection()
-								.prepareStatement(sTagQuery);
+					else {
+						while (rs.next()) {
+							String sTagQuery = "Select * from Tag where Id = "
+									+ Integer.parseInt(rs.getString("Id_Tag"));
+							PreparedStatement Tagquery = db.getConnection()
+									.prepareStatement(sTagQuery);
 
-						ResultSet Tagrs = Tagquery.executeQuery();
-						while (Tagrs.next()) {
-							String x = Tagrs.getString("Name");
-							dataset1.setValue(x,
-									Float.parseFloat(rs.getString("Hour")));
+							ResultSet Tagrs = Tagquery.executeQuery();
+							while (Tagrs.next()) {
+								String x = Tagrs.getString("Name");
+								dataset1.setValue(x,
+										Float.parseFloat(rs.getString("Hour")));
+							}
+							// Create the custom label generator
 						}
-						// Create the custom label generator
-
-						final PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator(
-								"{0} = {2}");
-						JFreeChart chart1 = ChartFactory.createPieChart(
-								"Weeks", dataset1, true, false, false);
-						PiePlot P = (PiePlot) chart1.getPlot();
-						P.setLabelGenerator(labelGenerator);
-						// P.setForegroundAlpha(0);
-						P.setCircular(true);
-						ChartPanel frame = new ChartPanel(chart1);
-						contentPane.setLayout(new java.awt.BorderLayout());
-						contentPane.add(frame, BorderLayout.SOUTH);
-						contentPane.validate();
 					}
+					final PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator(
+							"{0} = {2}");
+					JFreeChart chart1 = ChartFactory.createPieChart("Weeks",
+							dataset1, true, false, false);
+					PiePlot P = (PiePlot) chart1.getPlot();
+					P.setLabelGenerator(labelGenerator);
+					// P.setForegroundAlpha(0);
+					P.setCircular(true);
+					ChartPanel frame = new ChartPanel(chart1);
+					contentPane.setLayout(new java.awt.BorderLayout());
+					contentPane.add(frame, BorderLayout.SOUTH);
+					contentPane.validate();
+				}
 
-				} catch (Exception e1) {
+				catch (Exception e1) {
 					System.out.println(e1.toString());
 				}
 
-				
 			}
 		});
 
@@ -139,7 +140,7 @@ public class Statistics extends JFrame {
 		// years-------------------------------------------------*****************************************************************************************
 
 		JButton btnYears = new JButton("Show");
-		btnYears.setBounds(580, 73, 78, 29);
+		btnYears.setBounds(580, 73, 78, 25);
 		btnYears.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
@@ -178,40 +179,73 @@ public class Statistics extends JFrame {
 		});
 		contentPane.setLayout(null);
 
+		int startYear = 2000;
+		for (int i = 0; i < 100; i++) {
+			if (i == 0) {
+				year[i] = "Years";
+			} else
+				year[i] = "" + (startYear + i);
+
+		}
+		cbSta_Year = new JComboBox();
+		cbSta_Year.setModel(new DefaultComboBoxModel(year));
+		cbSta_Year.setBounds(500, 73, 70, 25);
+		contentPane.add(cbSta_Year);
+
 		contentPane.add(btnYears);
-		
-		JComboBox comboBox = new JComboBox();
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Weeks", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31", "32", "33", "34", "35", "36", "37", "38", "39", "40", "41", "42", "43", "44", "45", "46", "47", "48", "49", "50", "51", "52"}));
-		comboBox.setName("");
-		comboBox.setBounds(163, 73, 70, 29);
-		contentPane.add(comboBox);
-		
-		JComboBox comboBox_1 = new JComboBox();
-		comboBox_1.setModel(new DefaultComboBoxModel(new String[] {"Years", "2016", "2017", "2018", "2019", "2020"}));
-		comboBox_1.setBounds(500, 73, 70, 29);
-		contentPane.add(comboBox_1);
-		
+
 		JLabel lblStatistics = new JLabel("STATISTICS");
 		lblStatistics.setForeground(new Color(60, 179, 113));
 		lblStatistics.setHorizontalAlignment(SwingConstants.CENTER);
 		lblStatistics.setFont(new Font("Corbel", Font.BOLD, 25));
 		lblStatistics.setBounds(343, 11, 129, 29);
 		contentPane.add(lblStatistics);
-		
-		JComboBox comboBox_2 = new JComboBox();
-		comboBox_2.setModel(new DefaultComboBoxModel(new String[] {"Years", "2016", "2017", "2018", "2019", "2020"}));
-		comboBox_2.setBounds(83, 73, 70, 29);
-		contentPane.add(comboBox_2);
-		
-		JLabel lblShowDate = new JLabel("Show date");
-		lblShowDate.setBounds(163, 113, 164, 14);
-		contentPane.add(lblShowDate);
-		
+
+		// WEEK
+		cbWeek = new JComboBox();
+		cbWeek.setName("");
+		cbWeek.setBounds(83, 108, 220, 25);
+		contentPane.add(cbWeek);
+		cbYear = new JComboBox();
+		cbYear.setModel(new DefaultComboBoxModel(year));
+		cbYear.setBounds(83, 73, 95, 25);
+		contentPane.add(cbYear);
+		cbYear.addActionListener(new ActionListener() {
+
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					String iWeek1 = "";
+					String iWeek2 = "";
+					int iYearSelected = Integer.parseInt(cbYear
+							.getSelectedItem().toString());
+					for (int i = 0; i < 52; i++) {
+						if (i == 0) {
+							iWeek1 = Add_FirstWeek(iYearSelected);
+							week[i] = "WEEK " + (i + 1) + " : " + "1-1-"
+									+ iYearSelected + "->" + iWeek1;
+						} else {
+							week[i] = "WEEK " + (i + 1) + " : " + iWeek1 + "->"
+									+ Add_LasttWeek(iWeek1);
+							iWeek1 = Add_LasttWeek(iWeek1);
+						}
+					}
+
+					cbWeek.setModel(new DefaultComboBoxModel(week));
+
+				} catch (Exception e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+
+				}
+			}
+		});
+
 		JLabel lblByWeeks = new JLabel("By Weeks");
 		lblByWeeks.setFont(new Font("Corbel", Font.PLAIN, 14));
 		lblByWeeks.setBounds(83, 50, 70, 14);
 		contentPane.add(lblByWeeks);
-		
+
 		JLabel lblByYears = new JLabel("By Years");
 		lblByYears.setFont(new Font("Corbel", Font.PLAIN, 14));
 		lblByYears.setBounds(500, 50, 57, 14);
@@ -219,8 +253,38 @@ public class Statistics extends JFrame {
 		// tag in
 		// years-----------------------------------*****************************************************************************************
 
-		
-		
+	}
+
+	public static String Add_FirstWeek(int year) {
+		String untildate = "1-1-" + year;
+		String newday = "";
+		// current format
+		try {
+			SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(dateFormat.parse(untildate));
+			cal.add(Calendar.DATE, 7);
+			newday = dateFormat.format(cal.getTime());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return newday;
+	}
+
+	public static String Add_LasttWeek(String untildate) {
+		String newday = "";
+		// current format
+		try {
+			SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
+			Calendar cal = Calendar.getInstance();
+			cal.setTime(dateFormat.parse(untildate));
+			cal.add(Calendar.DATE, 7);
+			newday = dateFormat.format(cal.getTime());
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return newday;
 	}
 }
-
