2f4be213afcd5852dfdad95f5bb7229b342fed2b
nghianguyent133981 <Zed@10.11.17.85>
2016-07-15 15:35:03 +0700

    Update Statistics with Database #2

--- TrackingTime_Project/src/Statistics.java
+++ TrackingTime_Project/src/Statistics.java
@@ -33,13 +33,10 @@ import javax.swing.JLabel;
 import javax.swing.JPopupMenu;
 import java.awt.Component;
 
-
 public class Statistics extends JFrame {
 
 	private JPanel contentPane;
-	ResultSet rs;
 	DatabaseConnection db = new DatabaseConnection();
-	
 
 	/**
 	 * Launch the application.
@@ -71,80 +68,87 @@ public class Statistics extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 600, 500);
 		contentPane = new JPanel();
-		
-		//contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		// contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		//weeks -*--------------------------------------------********************************************************************************************
-		
+		// weeks
+		// -*--------------------------------------------********************************************************************************************
+
 		JButton btnWeeks = new JButton("Weeks");
 		btnWeeks.setBounds(146, 10, 84, 29);
-		
+
 		btnWeeks.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				
-				try {		
-					db.Connect();
-					PreparedStatement query = db.getConnection().prepareStatement(
-							"Select * from TimeLog");
+
+				int year = 2016;
+				int week = 1;
+				String sQuery = "Select * from Plans where Year=" + year
+						+ " and Week =" + week;
+				try {
+					DefaultPieDataset dataset1 = new DefaultPieDataset();
+					PreparedStatement query = db.getConnection()
+							.prepareStatement(sQuery);
 					ResultSet rs = query.executeQuery();
-				
-					
-					
-						
-						int hours = Integer.parseInt(rs.getString("Hour"));
-						//String x = new String();
-						//x = rs.getString("Name");
-						DefaultPieDataset dataset1 = new DefaultPieDataset();
-					    dataset1.setValue("fgh", hours);
-					    dataset1.setValue("abc", 25);
-					    dataset1.setValue("xyz", 25);
-					    
-					 // Create the custom label generator
-				        
-					    final PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator("{0} = {2}");
-					    JFreeChart chart1 = ChartFactory.createPieChart("Weeks", dataset1, true, false, false);
-					    PiePlot P = (PiePlot) chart1.getPlot();
-				        P.setLabelGenerator(labelGenerator);
-					    //P.setForegroundAlpha(0);
-					    P.setCircular(true);
-					    ChartPanel frame = new ChartPanel(chart1);
-			            contentPane.setLayout(new java.awt.BorderLayout());
-			            contentPane.add(frame, BorderLayout.SOUTH);
-					    contentPane.validate();
-					
-					
-				
+
+					// float hours = Float.parseFloat(rs.getString("Hours"));
+
+					while (rs.next()) {
+						String sTagQuery = "Select * from Tag where Id = "
+								+ Integer.parseInt(rs.getString("Id_Tag"));
+						PreparedStatement Tagquery = db.getConnection()
+								.prepareStatement(sTagQuery);
+
+						ResultSet Tagrs = Tagquery.executeQuery();
+						while (Tagrs.next()) {
+							String x = Tagrs.getString("Name");
+							dataset1.setValue(x,
+									Float.parseFloat(rs.getString("Hour")));
+						}
+						// Create the custom label generator
+
+						final PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator(
+								"{0} = {2}");
+						JFreeChart chart1 = ChartFactory.createPieChart(
+								"Weeks", dataset1, true, false, false);
+						PiePlot P = (PiePlot) chart1.getPlot();
+						P.setLabelGenerator(labelGenerator);
+						// P.setForegroundAlpha(0);
+						P.setCircular(true);
+						ChartPanel frame = new ChartPanel(chart1);
+						contentPane.setLayout(new java.awt.BorderLayout());
+						contentPane.add(frame, BorderLayout.SOUTH);
+						contentPane.validate();
+					}
+
 				} catch (Exception e1) {
-					JOptionPane.showMessageDialog(null, "No connection to server");
+					System.out.println(e1.toString());
 				}
-				
+
 				// lay du lieu tu db
-			/*	DefaultPieDataset dataset1 = new DefaultPieDataset();
-			    dataset1.setValue(tagname, 48);
-			    dataset1.setValue("gdfg", 25);
-			    dataset1.setValue("Work", 25);
-			    dataset1.setValue("Sleep", 25); 
-			 // Create the custom label generator
-		        final PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator("{0} = {2}");
-			    JFreeChart chart1 = ChartFactory.createPieChart("Weeks", dataset1, true, false, false);
-			    PiePlot P = (PiePlot) chart1.getPlot();
-		        P.setLabelGenerator(labelGenerator);
-			    //P.setForegroundAlpha(0);
-			    P.setCircular(true);
-			    ChartPanel frame = new ChartPanel(chart1);
-	            contentPane.setLayout(new java.awt.BorderLayout());
-	            contentPane.add(frame, BorderLayout.SOUTH);
-			    contentPane.validate();*/
+				/*
+				 * DefaultPieDataset dataset1 = new DefaultPieDataset();
+				 * dataset1.setValue(tagname, 48); dataset1.setValue("gdfg",
+				 * 25); dataset1.setValue("Work", 25);
+				 * dataset1.setValue("Sleep", 25); // Create the custom label
+				 * generator final PieSectionLabelGenerator labelGenerator = new
+				 * StandardPieSectionLabelGenerator("{0} = {2}"); JFreeChart
+				 * chart1 = ChartFactory.createPieChart("Weeks", dataset1, true,
+				 * false, false); PiePlot P = (PiePlot) chart1.getPlot();
+				 * P.setLabelGenerator(labelGenerator);
+				 * //P.setForegroundAlpha(0); P.setCircular(true); ChartPanel
+				 * frame = new ChartPanel(chart1); contentPane.setLayout(new
+				 * java.awt.BorderLayout()); contentPane.add(frame,
+				 * BorderLayout.SOUTH); contentPane.validate();
+				 */
 			}
-		}
-		);
-		
+		});
+
 		contentPane.setLayout(null);
 		contentPane.setLayout(null);
 		contentPane.add(btnWeeks);
-		//years-------------------------------------------------*****************************************************************************************
-		
+		// years-------------------------------------------------*****************************************************************************************
+
 		JButton btnYears = new JButton("Years");
 		btnYears.setBounds(242, 10, 78, 29);
 		btnYears.addActionListener(new ActionListener() {
@@ -155,85 +159,89 @@ public class Statistics extends JFrame {
 			@Override
 			public void mouseClicked(MouseEvent e) {
 				DefaultPieDataset dataset2 = new DefaultPieDataset();
-			    dataset2.setValue("Study", 23);
-			    dataset2.setValue("Relax", 75);
-			    dataset2.setValue("Work", 324);
-			    dataset2.setValue("Sleep", 23);
-			 // Create the custom label generator
-		        final PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator("{0} = {2}");
-			    JFreeChart chart2 = ChartFactory.createPieChart("Years", dataset2, true, false, false);
-			    PiePlot P = (PiePlot) chart2.getPlot();
-			    //P.setForegroundAlpha(0);
-		        P.setLabelGenerator(labelGenerator);
+				dataset2.setValue("Study", 23);
+				dataset2.setValue("Relax", 75);
+				dataset2.setValue("Work", 324);
+				dataset2.setValue("Sleep", 23);
+				// Create the custom label generator
+				final PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator(
+						"{0} = {2}");
+				JFreeChart chart2 = ChartFactory.createPieChart("Years",
+						dataset2, true, false, false);
+				PiePlot P = (PiePlot) chart2.getPlot();
+				// P.setForegroundAlpha(0);
+				P.setLabelGenerator(labelGenerator);
 
-			    P.setCircular(true);
-			    ChartPanel frame = new ChartPanel(chart2);
-			   // frame.setVisible(true);
-			   // frame.setBounds(100, 300, 300, 200);
-			    //frame.setSize(200,200);
-			    //contentPane.add(frame);
-			    contentPane.setLayout(new java.awt.BorderLayout());
-	            //contentPane.add(frame, BorderLayout.AFTER_LAST_LINE);
-	           //contentPane.add(frame, new Dimension (200,200));
-			    //frame.setSize(200, 200);
+				P.setCircular(true);
+				ChartPanel frame = new ChartPanel(chart2);
+				// frame.setVisible(true);
+				// frame.setBounds(100, 300, 300, 200);
+				// frame.setSize(200,200);
+				// contentPane.add(frame);
+				contentPane.setLayout(new java.awt.BorderLayout());
+				// contentPane.add(frame, BorderLayout.AFTER_LAST_LINE);
+				// contentPane.add(frame, new Dimension (200,200));
+				// frame.setSize(200, 200);
 
-			    contentPane.add(frame,BorderLayout.SOUTH);
-			    contentPane.validate();
+				contentPane.add(frame, BorderLayout.SOUTH);
+				contentPane.validate();
 			}
 		});
 		contentPane.setLayout(null);
 
 		contentPane.add(btnYears);
-		//tag in years-----------------------------------*****************************************************************************************
-		
+		// tag in
+		// years-----------------------------------*****************************************************************************************
+
 		JButton btnTagInYears = new JButton("Tag in Years");
 		btnTagInYears.setBounds(325, 10, 121, 29);
 		btnTagInYears.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				//create data
+				// create data
 				DefaultPieDataset dataset3 = new DefaultPieDataset();
-			    dataset3.setValue("Study", 324);
-			    dataset3.setValue("Relax", 234);
-			    dataset3.setValue("Work", 123);
-			    dataset3.setValue("Sleep", 890);
-			 // Create the custom label generator
-		        final PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator("{0} = {2}");
+				dataset3.setValue("Study", 324);
+				dataset3.setValue("Relax", 234);
+				dataset3.setValue("Work", 123);
+				dataset3.setValue("Sleep", 890);
+				// Create the custom label generator
+				final PieSectionLabelGenerator labelGenerator = new StandardPieSectionLabelGenerator(
+						"{0} = {2}");
 
-			    JFreeChart chart3 = ChartFactory.createPieChart("Tag in Years", dataset3, true, false, false);
-			    PiePlot P = (PiePlot) chart3.getPlot();
-		        P.setLabelGenerator(labelGenerator);
+				JFreeChart chart3 = ChartFactory.createPieChart("Tag in Years",
+						dataset3, true, false, false);
+				PiePlot P = (PiePlot) chart3.getPlot();
+				P.setLabelGenerator(labelGenerator);
 
+				// P.setForegroundAlpha(0);
+				P.setCircular(true);
+				ChartPanel frame = new ChartPanel(chart3);
 
-			    //P.setForegroundAlpha(0);
-			    P.setCircular(true);
-			    ChartPanel frame = new ChartPanel(chart3);
-			    
-			   // frame.setVisible(true);
-			    //frame.setBounds(500, 300, 300, 200);
+				// frame.setVisible(true);
+				// frame.setBounds(500, 300, 300, 200);
 
-			    //frame.setSize(300,200);
-			    contentPane.setLayout(new java.awt.BorderLayout());
-	            //contentPane.add(frame, BorderLayout.AFTER_LAST_LINE);
-	           // contentPane.add(frame, new Dimension (200,200));
-			   // frame.setSize(500, 500);
-			    
-			    contentPane.add(frame,BorderLayout.SOUTH);
-			    contentPane.validate();
-			    
-			    
+				// frame.setSize(300,200);
+				contentPane.setLayout(new java.awt.BorderLayout());
+				// contentPane.add(frame, BorderLayout.AFTER_LAST_LINE);
+				// contentPane.add(frame, new Dimension (200,200));
+				// frame.setSize(500, 500);
+
+				contentPane.add(frame, BorderLayout.SOUTH);
+				contentPane.validate();
+
 			}
 		});
 		contentPane.setLayout(null);
 
 		contentPane.add(btnTagInYears);
-		
+
 		JLabel lblAthena = new JLabel("athena");
 		lblAthena.setBounds(31, 25, 61, 16);
 		contentPane.add(lblAthena);
-		
+
 		JLabel lblAbc = new JLabel("abc");
 		lblAbc.setBounds(31, 106, 61, 16);
 		contentPane.add(lblAbc);
 	}
 }
+
