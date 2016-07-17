81cb136827aa2271b1b73693d9e297904259d755
letrieuhuan152 <letrieuhuan152@gmail.com>
2016-07-18 05:19:22 +0700

    5h sang

--- TrackingTime_Project/src/ManageTag.java
+++ TrackingTime_Project/src/ManageTag.java
@@ -2,12 +2,15 @@ import java.awt.EventQueue;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
@@ -21,9 +24,6 @@ import javax.swing.ListSelectionModel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-
 public class ManageTag extends JFrame implements ActionListener {
 	private JPanel contentPane;
 	private JTextField txt_tag_name;
@@ -31,96 +31,18 @@ public class ManageTag extends JFrame implements ActionListener {
 	DatabaseConnection db = new DatabaseConnection();
 	ResultSet rs;
 	JButton btn_tag_delete;
-	String selectedId;
+	String cbdata;
 	JComboBox cbb_tag_status;
 	int row;
-	int status;
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		JButton btemp = (JButton) e.getSource();
-		if (btemp.getActionCommand().equals("ADD")) {
+	int Status;
+	String SelectedId;
 
-		
-		boolean reslt = false;
-		String name = txt_tag_name.getText();
-		 //cbdata = cbb_tag_status.getSelectedItem();
-		Pattern patternname = Pattern.compile("^[a-zA-Z_0-9]+$");
-		Matcher matchername = patternname.matcher(name);
-		if (matchername.find()){
-			reslt = true;}
-		else {
-			reslt = false;}
-		if (reslt == true) {
-				
-					try {
-						if (cbb_tag_status.getSelectedItem().equals("True")) {
-							String queryS="Insert into Tag"
-									+ "(Name,Status)"
-									+"values"
-									+ "('"+name+"','1')";
-						PreparedStatement query = db.getConnection()
-								.prepareStatement(queryS);
-						query.executeUpdate();
-						table.repaint();
-						JOptionPane.showMessageDialog(null,"Thêm công việc thành công.", "Thành Công",  JOptionPane.INFORMATION_MESSAGE);
-						Load("Select * from Tag");
-						}
-						} catch (SQLException e1) {
-						// TODO Auto-generated catch block
-						e1.printStackTrace();
-					}
-				
-			
-		}}
-		if (btemp.getActionCommand().equals("UPDATE")) {
-			
-		}
-		if (btemp.getActionCommand().equals("DELETE")) {
-			btn_tag_delete.setEnabled(false);
-
-			if (JOptionPane.showConfirmDialog(null,
-					"B?n có ch?c ch?n mu?n xóa ho?t d?ng này?", "Xác Nh?n",
-					JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
-				txt_tag_name.setText(null);
-
-				try {
-
-					PreparedStatement query1 = db.getConnection()
-							.prepareStatement(
-									"Delete from Tag where Id='" + selectedId
-											+ "'");
-					query1.executeUpdate();
-
-					DefaultTableModel a = (DefaultTableModel) table.getModel();
-
-					while (a.getRowCount() > 0) {
-						for (int i = 0; i < a.getRowCount(); ++i) {
-							a.removeRow(i);
-						}
-					}
-					table.setModel(a);
-					Load("Select * from Tag");
-					JOptionPane.showMessageDialog(null,
-							"Xóa ho?t d?ng thành công", "Thành Công",
-							JOptionPane.INFORMATION_MESSAGE);
-				} catch (Exception e1) {
-					e1.printStackTrace();
-				}
-
-			}
-
-		}
-	}
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
+	public static void main(String[] a) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
 					ManageTag frame = new ManageTag("username");
+					
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -133,6 +55,7 @@ public class ManageTag extends JFrame implements ActionListener {
 	 * Create the frame.
 	 */
 	public ManageTag(String username) {
+		setAlwaysOnTop(true);
 
 		try {
 			db.Connect();
@@ -141,8 +64,13 @@ public class ManageTag extends JFrame implements ActionListener {
 			e.printStackTrace();
 		}
 		//
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 374, 492);
+		// setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		setTitle("ADD NEW TAGS");
+	
+		setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+		
+		
+		setBounds(100, 100, 373, 479);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -171,33 +99,39 @@ public class ManageTag extends JFrame implements ActionListener {
 		JComboBox cbb_tag_status = new JComboBox();
 		cbb_tag_status.setBounds(82, 140, 94, 25);
 		contentPane.add(cbb_tag_status);
-		cbb_tag_status.addItem("True");
-		cbb_tag_status.addItem("False");
-		/*if(cbb_tag_status.getSelectedItem().equals("True")){
-			 status=1;
-		}else{
-			status=0;
-		}*/
+		cbb_tag_status.insertItemAt("False",0);
+		cbb_tag_status.insertItemAt("True",1);
 		
+		/*
+		 * if(cbb_tag_status.getSelectedItem().equals("True")){ status=1; }else{
+		 * status=0; }
+		 */
+		ImageIcon iadd = new ImageIcon("Images/Managetag_add.png");
+		ImageIcon iaupdate = new ImageIcon("Images/Managetag_Update.png");
+		ImageIcon idelete = new ImageIcon("Images/Managetag_Delete.png");
 		txt_tag_name = new JTextField();
+
 		txt_tag_name.setBounds(82, 96, 137, 25);
 		contentPane.add(txt_tag_name);
 		txt_tag_name.setColumns(10);
 		// add buton
-		JButton btn_tag_add = new JButton("ADD");
-		btn_tag_add.setBounds(10, 196, 89, 38);
+		JButton btn_tag_add = new JButton(iadd);
+		btn_tag_add.setBounds(10, 176, 90, 71);
 		contentPane.add(btn_tag_add);
+		
 		// add action for btn add
 		btn_tag_add.addActionListener(this);
 
-		JButton btn_tag_update = new JButton("UPDATE");
-		btn_tag_update.setBounds(109, 196, 89, 39);
+		JButton btn_tag_update = new JButton(iaupdate);
+		
+		btn_tag_update.setBounds(111, 176, 86, 71);
 		contentPane.add(btn_tag_update);
 		// /add action for btn update
 		btn_tag_update.addActionListener(this);
 
-		JButton btn_tag_delete = new JButton("DELETE");
-		btn_tag_delete.setBounds(208, 196, 89, 39);
+		JButton btn_tag_delete = new JButton(idelete);
+		
+		btn_tag_delete.setBounds(207, 176, 90, 71);
 		contentPane.add(btn_tag_delete);
 		// add action for btn delete
 		btn_tag_delete.addActionListener(this);
@@ -210,16 +144,16 @@ public class ManageTag extends JFrame implements ActionListener {
 		table.addMouseListener(new MouseAdapter() {
 			@Override
 			public void mouseClicked(MouseEvent e) {
-				
+
 				btn_tag_delete.setEnabled(true);
 				btn_tag_update.setEnabled(true);
 				row = table.getSelectedRow();
 				DefaultTableModel model = (DefaultTableModel) table.getModel();
 				String SelectedName = model.getValueAt(row, 1).toString();
-				String cbbselect=model.getValueAt(row,2).toString();
-				txt_tag_name.setText(SelectedName);
-				//cbb_tag_status.setSelectedItem(cbbselect);
-				selectedId = (model.getValueAt(row, 0)).toString();
+				String cbbselect = model.getValueAt(row, 2).toString();
+				txt_tag_name.setText(SelectedName);//
+				cbb_tag_status.setSelectedItem(cbbselect);
+				SelectedId = (model.getValueAt(row, 0)).toString();
 			}
 		});
 		table.setRowSelectionAllowed(true);
@@ -231,20 +165,7 @@ public class ManageTag extends JFrame implements ActionListener {
 		table.setSelectionMode(ListSelectionModel.MULTIPLE_INTERVAL_SELECTION);
 
 		// connect data
-		PreparedStatement query;
-		try {
-			query = db.getConnection().prepareStatement("Select * from Tag");
-			ResultSet rs = query.executeQuery();
-			while (rs.next()) {
-				// cbb_tag_status.addItem(rs.getString("0"));
-				// .addItem(rs.getString("1"));
-			}
-		} catch (SQLException e) {
-			JOptionPane.showMessageDialog(null, "No connection to server",
-					"Error", JOptionPane.ERROR_MESSAGE);
-			e.printStackTrace();
-		}
-
+		
 	}
 
 	public void Load(String queryExe) {
@@ -265,6 +186,128 @@ public class ManageTag extends JFrame implements ActionListener {
 			table.setModel(model);
 		} catch (Exception e1) {
 			e1.printStackTrace();
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JButton btemp = (JButton) e.getSource();
+		
+		if (btemp.getActionCommand().equals("ADD")) {
+			DefaultTableModel model = (DefaultTableModel) table.getModel();
+			boolean reslt = false;
+			String name = txt_tag_name.getText();
+			row = table.getSelectedRow();
+			SelectedId = (model.getValueAt(row, 2)).toString();
+			// String cb1=cbb_tag_status.getSelectedItem().toString();
+			 
+			Pattern patternname = Pattern.compile(".*\\D.*");
+			Matcher matchername = patternname.matcher(name);
+			if (matchername.find()) {
+				reslt = true;
+			} else {
+				reslt = false;
+			}
+			if (reslt == true) {
+			//	if(cb1==("0")){
+				try {
+
+					String sql = "Insert into Tag" + "(Name,Status)" + "values" + "('"+name+"','"+SelectedId+"')";
+					PreparedStatement query = db.getConnection()
+							.prepareStatement(sql);
+					query.executeUpdate();
+					table.repaint();
+					//
+					JOptionPane.showMessageDialog(null,
+							"Thêm công việc thành công.", "Thành Công",
+							JOptionPane.INFORMATION_MESSAGE);
+					Load("Select * from Tag");
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+					JOptionPane.showMessageDialog(null,"Vui lòng nhập tên hoạt động cần thêm");
+					 }
+				}
+
+		}
+		
+		if (btemp.getActionCommand().equals("UPDATE")) {
+			boolean reslt = false;
+			String name = txt_tag_name.getText();
+			//row = table.getSelectedRow();
+			//SelectedId = (model.getValueAt(row, 2)).toString();
+		//	 String cb1=cbb_tag_status.getSelectedItem().toString();
+			 
+			Pattern patternname = Pattern.compile(".*\\D.*");
+			Matcher matchername = patternname.matcher(name);
+			if (matchername.find()) {
+				reslt = true;
+			} else {
+				reslt = false;
+			}
+			if (reslt == true) {
+				
+				try {
+					PreparedStatement query = db.getConnection()
+							.prepareStatement(
+									"Update Tag set Name='" + name
+											+ "',Status='" + 0
+											+ "' where id ='" + SelectedId
+											+ "'");
+					query.executeUpdate();
+					table.repaint();
+					JOptionPane.showMessageDialog(null,"Sửa công việc thành công.", "Thành Công",  JOptionPane.INFORMATION_MESSAGE);
+					Load("Select * from Tag");
+					txt_tag_name.setText(null);
+					
+				} catch (SQLException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+					 }}
+				}
+
+			 else {
+				JOptionPane.showMessageDialog(null,
+						"Vui lòng nhập tên hoạt động cần thêm");
+			
+		}
+		if (btemp.getActionCommand().equals("DELETE")) {
+
+			if (JOptionPane.showConfirmDialog(null,
+					"Bạn có chắc muốn xóa hoạt động này?", "Xác Nhận",
+					JOptionPane.YES_NO_OPTION) == JOptionPane.YES_OPTION) {
+				int row = table.getSelectedRow();
+				DefaultTableModel a = (DefaultTableModel) table.getModel();
+
+				String selected = a.getValueAt(row, 0).toString();
+				if (txt_tag_name.getText() != null) {
+					if (row >= 0) {
+
+						a.removeRow(row);
+
+						try {
+
+							PreparedStatement ps = db.getConnection()
+									.prepareStatement(
+											"delete from Tag where Id='"
+													+ selected + "' ");
+							ps.executeUpdate();
+
+							table.setModel(a);
+							Load("Select * from Tag");
+							JOptionPane.showMessageDialog(null,
+									"Xóa hoạt động thành công", "Thành Công",
+									JOptionPane.INFORMATION_MESSAGE);
+						} catch (Exception e1) {
+							e1.printStackTrace();
+						}
+					}
+				} else {
+					JOptionPane.showMessageDialog(null,
+							"Vui Lòng chọn tên hoạt động cần xóa", "Lỗi",
+							JOptionPane.ERROR_MESSAGE);
+				}
+			}
 		}
 	}
 }
